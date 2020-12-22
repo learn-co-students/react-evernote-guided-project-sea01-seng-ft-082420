001b884c3b12766e7443a4310b65773811e8db80
jonelle-noelani <jonelle.noelani@gmail.com>
2020-12-21 19:24:16 -0800

    updated routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
       resources :users
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
